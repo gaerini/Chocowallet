d99e949c72f75ba1424031c0d34179a823ea4ae4
jiho Kim <jimmy7335@gmail.com>
2023-06-26 12:07:04 +0900

    intial settings

--- project/app/models.py
+++ project/app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Event(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='users')
+    content = models.CharField(max_length=50)
+    date = models.DateField()
+    cost = models.IntegerField()
+    
