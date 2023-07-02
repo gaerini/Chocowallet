d6bdc95722ff70cfe808d8e62840e32789f305a2
jiho Kim <jimmy7335@gmail.com>
2023-07-02 23:02:37 +0900

    sum cost

---
+++ project/app/static/js/eventSum.js
@@ -0,0 +1,18 @@
+
+function costSum () {
+    var sum = 0;
+    const monthSpan = document.querySelector("#calMonth");
+    events.forEach(function(obj){
+        const month = Number(monthSpan.innerText);
+        const eventDate = new Date(obj.finish_date);
+        console.log(eventDate.getMonth());
+        if (month == eventDate.getMonth()+1) {
+            
+            sum = sum + obj.cost;
+        }
+    });
+    
+    const expectedCost = document.querySelector(".span_cost");
+    
+    expectedCost.innerText = sum;
+}
