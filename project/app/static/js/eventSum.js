f909df78fb82fb05e4accd1880d220842cf442e6
jeongseokmandoo <127060327+jeongseokmandoo@users.noreply.github.com>
2023-07-04 18:23:15 +0900

    progressbar_js

--- project/app/static/js/eventSum.js
+++ project/app/static/js/eventSum.js
@@ -1,7 +1,8 @@
+var sum = 0;
+var realsum = 0;
+const monthSpan = document.querySelector("#calMonth");
 
 function costSum () {
-    var sum = 0;
-    const monthSpan = document.querySelector("#calMonth");
     events.forEach(function(obj){
         const month = Number(monthSpan.innerText);
         const eventDate = new Date(obj.finish_date);
@@ -14,5 +15,32 @@ function costSum () {
     
     const expectedCost = document.querySelector("#expected_cost");
     
-    expectedCost.innerText = sum;
+    expectedCost.innerText = sum + '원';
+}
+
+function costRealSum () {
+    realspends.forEach(function(i){
+        const mon = Number(monthSpan.innerText);
+        const spenddate = i.date.substring(4, 6);
+        const spendDate = Number(spenddate);
+
+        if (mon == spendDate) {
+
+            realsum = realsum + i.spend;
+        }
+    });
+
+    const realCost = document.querySelector("#spent_cost");
+
+    realCost.innerText = realsum + '원';
+}
+
+function costRatio () {
+    const ratio = sum !== 0 ? (realsum / sum) * 100 : 0;
+    if (ratio > 100) {
+        ratio = 100;
+    }
+    
+    const progressPercentElement = document.querySelector(".progresspercent");
+    progressPercentElement.style.width = `${ratio}%`;
 }
